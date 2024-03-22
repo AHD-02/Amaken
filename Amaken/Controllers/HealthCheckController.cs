7fd1482c06ffe8de31f47d7469a3253a2be87699
Ahd <pc@DESKTOP-G62OAET>
2024-03-22 16:38:10 +0300

    Add health check

---
+++ Amaken/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amaken.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthCheckController: ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
